a18b4dc7c0e88bbcfa07e22da9f42d91a3552c73
John Abraham <johnjabraham@live.com>
2023-12-15 12:50:42 +1100

    added billing API

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -1,22 +1,24 @@
-import { Api, StackContext, use } from "sst/constructs";
+import { Api, Config, StackContext, use } from "sst/constructs";
 import { StorageStack } from "./StorageStack";
 
 export function ApiStack( {stack}: StackContext) {
+    const STRIPE_SECRET_KEY = new Config.Secret(stack, "STRIPE_SECRET_KEY")
     const  {table} = use(StorageStack);
 
     const api = new Api(stack, "Api", {
         defaults: {
             authorizer: "iam",
             function: {
-                bind: [table],
+                bind: [table, STRIPE_SECRET_KEY],
             }
         },
         routes: {
-            "POST /notes": "packages/functions/src/create.main",
-            "GET /notes/{id}": "packages/functions/src/get.main",
-            "GET /notes": "packages/functions/src/list.main",
-            "PUT /notes/{id}": "packages/functions/src/update.main",
+            "POST   /notes": "packages/functions/src/create.main",
+            "GET    /notes/{id}": "packages/functions/src/get.main",
+            "GET    /notes": "packages/functions/src/list.main",
+            "PUT    /notes/{id}": "packages/functions/src/update.main",
             "DELETE /notes/{id}": "packages/functions/src/delete.main",
+            "POST   /billing": "packages/functions/src/billing.main",
         }
     })
 
